12cc5b3f6e903cb729a57903f6ba36edcdbd839f
Nick Cellini <ncellini@google.com>
2020-04-19 02:24:37 +1000

    Started to create a bespoke level design. The loadLevelData function takes an array of objects that define what the level consists of.

--- game.js
+++ game.js
@@ -1,33 +1,40 @@
+var renderStyle = {
+  "antigrav" : { fill: "blue", stroke: "transparent" },
+  "wall" : { fill: "#000", stroke: "transparent" },
+  "ground" : { fill: "#000", stroke: "transparent" }
+};
+
 var lvlData1 = [
-  { shape: "circle", pos: [0, 0], radius: 8 },
-  { shape: "circle", pos: [0, 0], radius: 8 },
-  { shape: "rect", pos: [0, 0], size: [0, 0] }
+  { shape: "circle", type: "antigrav", pos: [11, 0], radius: 9 },
+  { shape: "circle", type: "antigrav", pos: [-10, 24], radius: 12 },
+  { shape: "circle", type: "antigrav", pos: [-30, 40], radius: 8 },
+
+  { shape: "circle", type: "antigrav", pos: [-36, 67], radius: 3 },
+  { shape: "circle", type: "antigrav", pos: [-35, 79], radius: 3.5 },
+  { shape: "circle", type: "antigrav", pos: [-22, 77], radius: 7 },
+  { shape: "circle", type: "antigrav", pos: [-38, 92], radius: 5 },
+  { shape: "circle", type: "antigrav", pos: [-28, 110], radius: 4 },
+
+  // { shape: "rect", type: "antigrav", pos: [-30, 30], size: [1, 3] },
+  // { shape: "circle", type: "antigrav", pos: [-2, 3], radius: 8 },
+  { shape: "rect", type: "wall", pos: [31.2, 62], size: [0.8, 32], rotation: 0 },
+  { shape: "circle", type: "antigrav", pos: [40, 30], radius: 8 }, // HARD path1
+  { shape: "rect", type: "antigrav", pos: [40, 62], size: [8, 32], rotation: 0 }, // // HARD path1
+  { shape: "circle", type: "antigrav", pos: [40, 94], radius: 8 },
+  // { shape: "polygon", type: "wall", pos: [-2, 3], vertices: [1, 2] }
+
+  { shape: "circle", type: "antigrav", pos: [-10, 136], radius: 17 },
+
+  { shape: "rect", type: "ground", pos: [-31, 162], size: [10, 2], rotation: 0 },
+  // ===========================================================================
+  
+
+
 ];
 
 var hero;
-var jellies = [];
-
-function loadRandomCircles({ground}) {
-  for (let h = 0; h < 10; h++) {
-    for (let i = 0; i < 10; i++) {
-      var pos = Vec2(-25 + Math.random() * 50, Math.random() * 100 + h * 100);
-      pos.y += 65;
-      let j = ground.createFixture({
-        shape: pl.Circle(pos, 8.0),
-        isSensor: true
-      });
-      j.render = { fill: "blue", stroke: "transparent" };
-      j.gameObjectType = "jelly";
-      jellies.push(j);
-    }
-  }
-}
-
-function loadLevelData({lvlDat, ground}) {
-  lvlDat.forEach(element => {
-
-  });
-}
+var antigravList = [];
+var groundFixtures = [];
 
 planck.testbed(function(testbed) {
   // testbed.background = "#222222";
@@ -49,48 +56,42 @@ planck.testbed(function(testbed) {
   let groundSize = Vec2(48, 64);
   var ground = world.createBody(Vec2(0,-88));
   let groundFixture = ground.createFixture(pl.Box(groundSize.x, groundSize.y), 1.0);
-  groundFixture.render = { fill: "#000", stroke: "transparent" };
+  groundFixture.render = renderStyle['ground'];
+  groundFixtures.push(groundFixture);
   
   let wallHeight = 2047; // 2048 is the max for some reason?
   let wallStyle = { fill: "000", stroke: "transparent" };
-  ground.createFixture(pl.Box(2, wallHeight, Vec2(groundSize.x-2, wallHeight+groundSize.y-1)), 1.0)
-    .render = { fill: "000", stroke: "transparent" };
-  ground.createFixture(pl.Box(2, wallHeight, Vec2(2-groundSize.x, wallHeight+groundSize.y-1)), 1.0)
-    .render = { fill: "000", stroke: "transparent" };
+  ground.createFixture(pl.Box(4, wallHeight, Vec2(groundSize.x-4, wallHeight+groundSize.y-1)), 1.0)
+    .render = wallStyle;
+  ground.createFixture(pl.Box(4, wallHeight, Vec2(4-groundSize.x, wallHeight+groundSize.y-1)), 1.0)
+    .render = wallStyle;
 
-  loadRandomCircles({ground: ground});
-  // loadLevelData({ lvlDat: lvlData1, ground: ground });
-
-
-  // for (var i = 0; i < COUNT; ++i) {
-  //     if (i == 3) continue;
-  //     touching[i] = { touching: false };
-
-  //     bodies[i] = world.createDynamicBody(Vec2(-10.0 + 3.0 * i, 20.0));
-  //     bodies[i].setUserData(touching[i])
-  //     bodies[i].createFixture(circle, 1.0);
-  // }
+  // -------------------------------------
+    // loadRandomCircles({ground: ground, groundHeight:groundSize.y});
+  loadLevelData({ lvlDat: lvlData1, ground: ground, groundHeight:groundSize.y });
+  // -------------------------------------
 
   // Implement contact listener.
   world.on("begin-contact", function(contact) {
     var fixtureA = contact.getFixtureA();
     var fixtureB = contact.getFixtureB();
 
-    if (
-      (fixtureA == hero.fixture && fixtureB == groundFixture) ||
-      (fixtureB == hero.fixture && fixtureA == groundFixture)
-    ) {
-      hero.onGround = true;
+    for (let i = 0; i < groundFixtures.length; i++) {
+      if (groundFixtures[i] == fixtureA && fixtureB === hero.fixture ||
+          groundFixtures[i] == fixtureB && fixtureA === hero.fixture) {
+        hero.onGround = true;
+        break;
+      }
     }
 
-    if (fixtureA.gameObjectType === 'jelly') {
+    if (fixtureA.gameObjectType === 'antigrav') {
       var userData = fixtureB.getBody().getUserData();
       if (userData) {
         userData.touching += 1;
       }
     }
 
-    if (fixtureB.gameObjectType === 'jelly') {
+    if (fixtureB.gameObjectType === 'antigrav') {
       var userData = fixtureA.getBody().getUserData();
       if (userData) {
         userData.touching += 1;
@@ -103,21 +104,22 @@ planck.testbed(function(testbed) {
     var fixtureA = contact.getFixtureA();
     var fixtureB = contact.getFixtureB();
 
-    if (
-      (fixtureA === hero.fixture && fixtureB === groundFixture) ||
-      (fixtureB === hero.fixture && fixtureA === groundFixture)
-    ) {
-      hero.onGround = false;
+    for (let i = 0; i < groundFixtures.length; i++) {
+      if (groundFixtures[i] == fixtureA && fixtureB === hero.fixture ||
+          groundFixtures[i] == fixtureB && fixtureA === hero.fixture) {
+        hero.onGround = false;
+        break;
+      }
     }
 
-    if (fixtureA.gameObjectType === 'jelly') {
+    if (fixtureA.gameObjectType === 'antigrav') {
       var userData = fixtureB.getBody().getUserData();
       if (userData) {
         userData.touching -= 1;
       }
     }
 
-    if (fixtureB.gameObjectType === 'jelly') {
+    if (fixtureB.gameObjectType === 'antigrav') {
       var userData = fixtureA.getBody().getUserData();
       if (userData) {
         userData.touching -= 1;
@@ -183,4 +185,52 @@ document.addEventListener("keydown", e => {
 
 function lerp(start, end, amt) {
   return (1 - amt) * start + amt * end;
+}
+
+function loadRandomCircles({ ground, groundHeight }) {
+  for (let h = 0; h < 10; h++) {
+    for (let i = 0; i < 10; i++) {
+      var pos = Vec2(-25 + Math.random() * 50, Math.random() * 100 + h * 100);
+      pos.y += groundHeight;
+      let j = ground.createFixture({
+        shape: pl.Circle(pos, 8.0),
+        isSensor: true
+      });
+      j.render = { fill: "blue", stroke: "transparent" };
+      j.gameObjectType = "antigrav";
+      antigravList.push(j);
+    }
+  }
+}
+
+function loadLevelData({ lvlDat, ground, groundHeight }) {
+  lvlDat.forEach(element => {
+    var newObj = null;
+    let pos = Vec2(element.pos[0], element.pos[1] + groundHeight);
+    let shape;
+    console.log(element);
+
+    if (element.shape === "circle") {
+      shape = pl.Circle(pos, element.radius);
+    } else if (element.shape === "rect") {
+      shape = pl.Box(element.size[0], element.size[1], pos);
+    } else {
+      var verts = Vec2[element.vertices.size];
+      // TODO: Populate the array
+      shape = pl.Polygon(verts);
+    }
+
+    if (element.type === "antigrav") {
+      newObj = ground.createFixture({ shape: shape, isSensor: true });
+      antigravList.push(newObj);
+    } else if (element.type === "wall" || element.type === "ground") {
+      newObj = ground.createFixture({ shape: shape }, 1.0);
+      groundFixtures.push(newObj);
+    }
+
+    if (newObj != null) {
+      newObj.gameObjectType = element.type;
+      newObj.render = renderStyle[element.type];
+    }
+  });
 }
